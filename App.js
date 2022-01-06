b524404364b1d5a5f0c7fc018d723d9675267376
YunJehyeok <dbswpgur2@naver.com>
2022-01-06 22:59:00 +0900

    1/6

--- App.js
+++ App.js
@@ -1,25 +1,58 @@
 import AppLoading from 'expo-app-loading';
-import React, { useState } from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { useAssets } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
+
+// const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font))
+// const loadImages = (images) => images.map((image) => {
+//   if(typeof image === 'string') {
+//     return Image.prefetch(image);
+//   } else {
+//     return Asset.loadAsync(image);
+//   }
+// })
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
+  // Hooks 없이 사용하기
 
-  const startLoading = async () => {
-    // 여기가 await 해야 되는 부분
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-  };
+  // const [ready, setReady] = useState(false);
+  // const onFinish = () => setReady(true);
 
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  // const startLoading = async () => {
+  //   const fonts = loadFonts([Ionicons.font]);
+  //   const images = loadImages([require('./my-face.jpeg')]);
+
+  //   await Promise.all([...fonts, ...images])
+  // };
+
+  // if (!ready) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={startLoading}
+  //       onFinish={onFinish}
+  //       onError={console.error}
+  //     />
+  //   );
+  // }
+
+  /////////////////////////////////////
+  // Hooks 사용하기
+  // 단, Image.prefetch와 같이 외부에서 뭘 받아올 수는 없으며
+  // 로딩 함수에서 뭘 수행할 수는 없다.
+  // 즉, AppLoading이 하는 일이 assets 로딩밖에 없다면 Hook을 사용
+  const [assets] = useAssets([require('./my-face.jpeg')]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
 
-  return <Text>We are done Loading</Text>;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
---
+++ navigation/Tabs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Movies from '../screens/Movie';
+import Tv from '../screens/Tv';
+import Search from '../screens/Search';
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+}
+
+export default Tabs;
